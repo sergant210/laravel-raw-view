4c7d33b4f255ca8962bf99fe8e347be58e5b7c72
sergant210 <sergant210@bk.ru>
2020-09-30 16:33:36 +0300

    v2.0.0

--- src/RawBladeCompiler.php
+++ src/RawBladeCompiler.php
@@ -30,7 +30,7 @@ class RawBladeCompiler extends BladeCompiler
      */
     public function getCompiledPath($path)
     {
-        return $this->cachePath.'/'.sha1($path).'.php';
+        return $this->cachePath . '/' . sha1($path) . '.php';
     }
 
     /**
--- tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-/**
- * Helpers files autoloader for tests
- */
-require_once __DIR__ . '/../src/helpers.php';
